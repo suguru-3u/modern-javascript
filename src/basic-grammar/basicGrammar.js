1d892179d8ca427fa2d7dc51aa2517fbb57d6300
suguru-3u <suguru.13579515@gmail.com>
2022-11-18 09:19:47 +0900

    study JS

--- src/basic-grammar/basicGrammar.js
+++ src/basic-grammar/basicGrammar.js
@@ -1070,3 +1070,27 @@ for (const match of matchesIterator) {
 // 次の順番でコンソールに出力される
 // match: "ABC", index: 0, input: "ABC あいう DE えお"
 // match: "DE", index: 8, input: "ABC あいう DE えお"
+
+// * 文字列の置換/削除 *
+// 検索対象となる文字列
+const str50 = "にわにはにわにわとりがいる";
+// 文字列を指定した場合は、最初に一致したものだけが置換される
+console.log(str50.replace("にわ", "niwa")); // => "niwaにはにわにわとりがいる"
+// `g`フラグなし正規表現の場合は、最初に一致したものだけが置換される
+console.log(str50.replace(/にわ/, "niwa")); // => "niwaにはにわにわとりがいる"
+// `g`フラグあり正規表現の場合は、繰り返し置換を行う
+console.log(str50.replace(/にわ/g, "niwa")); // => "niwaにはniwaniwaとりがいる"
+
+// replaceメソッドとreplaceAllメソッドでは、キャプチャした文字列を利用して複雑な置換処理もできます。
+function toDateJa(dateString) {
+  // パターンにマッチしたときのみ、コールバック関数で置換処理が行われる
+  return dateString.replace(/(\d{4})-(\d{2})-(\d{2})/g, (all, year, month, day) => {
+      // `all`には、マッチした文字列全体が入っているが今回は利用しない
+      // `all`が次の返す値で置換されるイメージ
+      return `${year}年${month}月${day}日`;
+  });
+}
+// マッチしない文字列の場合は、そのままの文字列が返る
+console.log(toDateJa("本日ハ晴天ナリ")); // => "本日ハ晴天ナリ"
+// マッチした場合は置換した結果を返す
+console.log(toDateJa("今日は2017-03-01です")); // => "今日は2017年03月01日です"
